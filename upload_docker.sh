43c1c07e1b24a32f1200928a14353950cc9cb5b0
Danilo Chaves <bills@danilochaves.com>
2019-10-22 22:37:46 -0400

    updated files

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,17 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=dchaves30/project_05:v1.0
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: " $dockerpath
+echo "Type your password: " 
+docker login -u dchaves30 --password-stdin
+
+#create repository
+docker tag udacity_project_05:1.0 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
