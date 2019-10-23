205723d682adc6337eeaccefa230a352267b20ce
Danilo Chaves <bills@danilochaves.com>
2019-10-22 20:19:52 -0400

    Task 3 Complete

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,16 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+echo Building Docker image...
+docker build --tag=udacity_project_05:1.0 .
+
 
 # Step 2: 
 # List docker images
+docker image ls
+
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 udacity_project_05:1.0
+
