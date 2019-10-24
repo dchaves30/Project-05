40d7868a4ad757e78f23e50b9bf2516d9089d722
Danilo Chaves <bills@danilochaves.com>
2019-10-24 14:47:47 -0400

    new files added

---
+++ make_prediction_kubernetes.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+export NODE_PORT=$(kubectl get services/project05 -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo "Port: $NODE_PORT"
+
+# POST method predict
+curl -d '{  
+   "CHAS":{  
+      "0":0
+   },
+   "RM":{  
+      "0":6.575
+   },
+   "TAX":{  
+      "0":296.0
+   },
+   "PTRATIO":{  
+      "0":15.3
+   },
+   "B":{  
+      "0":396.9
+   },
+   "LSTAT":{  
+      "0":4.98
+   }
+}'\
+     -H "Content-Type: application/json" \
+     -X POST http://$(minikube ip):$NODE_PORT/predict
